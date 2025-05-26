ffa7b4db7a03448b372dd5b571064e46884a13e4
Viktor Momotenko <for.scrimba@gmail.com>
2025-05-26 13:57:41 +0100

    fixed Reset button

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -38,7 +38,11 @@ export default function App() {
     <div className={css.app}>
       <CafeInfo />
 
-      <VoteOptions onVote={handleVote} onReset={resetVotes} canReset={true} />
+      <VoteOptions
+        onVote={handleVote}
+        onReset={resetVotes}
+        canReset={totalVotes > 0}
+      />
 
       {totalVotes > 0 ? (
         <VoteStats
